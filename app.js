2a1732293f4199586fa5e00d0e5677dfdbbada79
Ahmad Alhalwany <ahmedreer438i776666@outlook.com>
2026-03-08 20:33:42 +0300

    ههخ

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const apiBaseUrl = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:') 
     ? 'http://localhost:5000/api' 
-    : 'https://eyf-website-backend.onrender.com/api';
+    : '/api';
 
 function getTypeArabic(type) {
     const types = {
